654d5baf33e4a77d2f04190e4dd3c0130748f433
James Beaumont <james-beaumont@live.nmit.ac.nz>
2022-03-09 14:15:09 +1300

    Further changes to NOTE

--- src/app.js
+++ src/app.js
@@ -7,7 +7,9 @@ console.log("James Beaumont")
 Created a 'src' folder, and navigated to 'src' using 'cd src'
 Created new file 'app.js' in src folder 
 Created package.json file using 'npm init -y'
+Added "app.js" to package.json under "Main":
 Created new branch for the respective week of study, in this case week-3 
 Made changes in app.js
 Added, commited and pushed commits to branch just created
+Added "dev": "nodemon ./src/app.js", to package.json under "scripts"
 */
