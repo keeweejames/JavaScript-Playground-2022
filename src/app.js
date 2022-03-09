91da4dce8a21e99d128330acc8b33258f88d0fde
James Beaumont <james-beaumont@live.nmit.ac.nz>
2022-03-09 14:10:20 +1300

    Edit NOTE in app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,13 @@
-console.log("Hello World!")
+console.log("Hello")
+console.log("James Beaumont")
+//TODO
+
+//NOTE
+/* 
+Created a 'src' folder, and navigated to 'src' using 'cd src'
+Created new file 'app.js' in src folder 
+Created package.json file using 'npm init -y'
+Created new branch for the respective week of study, in this case week-3 
+Made changes in app.js
+Added, commited and pushed commits to branch just created
+*/
