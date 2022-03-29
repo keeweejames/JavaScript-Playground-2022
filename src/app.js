d75cd1a2f1863de8d113fc609cad5986231e2054
James Beaumont <james-beaumont@live.nmit.ac.nz>
2022-03-30 10:36:58 +1300

    Console

--- src/app.js
+++ src/app.js
@@ -1,9 +1,3 @@
-<div>What is your name?
-    <input type = "text">
-    <button>Send</button>
-</div>
-<script>
-document.querySelector("button".addEventListener("click", function (){
-    document.querySelector("div").textContent.Content = "Hello, " + document.querySelector("input").value;
-})
-</script>
+console.log("hello")
+var a = "hello"
+console.log(a);
