df0d07b72118219b0c2e29bd63d820095e480424
James Beaumont <james-beaumont@live.nmit.ac.nz>
2022-03-30 11:43:17 +1300

    Playing around

--- src/app.js
+++ src/app.js
@@ -5,8 +5,19 @@ console.log(a);
 var myName = "James"
 var myCourse = "JavaScript"
 console.log(myName);
-alert("welcome");
-var myAge = 45;
+//alert("welcome");
+var myAge = "45";
 console.log(myAge);
 
 //When adding JavaScript to hmtl, ./src/app.js needs to be pointing to path of file
+/*Add comments over
+multiple lines
+is done with these.
+*/
+
+var myWelcome = "Welcome" //Declaring a variable "myWelcome" with the value "Welcome"
+myWelcome = "You've outstayed your welcome" //Changing the variable "myWelcome"
+console.log(myWelcome); //Outputting new variable "myWelcome"
+var mySurname = "Beaumont"
+mySurname = "BEAUMONT"
+console.log(mySurname)
