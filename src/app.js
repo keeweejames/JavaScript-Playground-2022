af3a90397b779a0db0a8394f0941c26fd96f0dde
James Beaumont <james-beaumont@live.nmit.ac.nz>
2022-03-30 10:53:17 +1300

    More week-6 goodness

--- src/app.js
+++ src/app.js
@@ -1,3 +1,10 @@
 console.log("hello")
 var a = "hello"
 console.log(a);
+
+var myName = "James"
+var myCourse = "JavaScript"
+console.log(myName);
+alert("welcome");
+var myAge = 45;
+console.log(myAge);
