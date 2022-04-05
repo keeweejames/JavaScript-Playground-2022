05c20f3990e7f6b517123264c6461c1d755acb04
James Beaumont <james-beaumont@live.nmit.ac.nz>
2022-04-06 10:33:33 +1200

    Test

---
+++ src/app.js
@@ -0,0 +1,20 @@
+//create a function that takes array containing only numbers and return thr first element.
+//create a function named onlynumbers
+
+// function onlyNumbers(arr){}
+//     return arr[0]
+//     }
+//     console.log(onlyNumbers([1, 2, 3, 4]))
+
+//or
+
+// const onlyNumbers = arr => arr[0];
+// console.log(onlyNumbers([1, 2, 3, 4]))
+
+//give two arguments, return an array which contains these two arguments
+
+function createArray(a, b) {
+    var arr = [a, b]
+    return arr
+}
+console.log(createArray(1, 2))
